ac60fb9febc07f9363fb26bba0dc8d6d943b2a4d
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2022-03-22 15:17:12 +0000

    azure credential variables

--- environments/integration/state.tf
+++ environments/integration/state.tf
@@ -1,14 +1,19 @@
 terraform {
   required_version = "~> 0.13.0"
   required_providers {
-    azurerm = {
-      version = "~> 2.94"
-      features = {}
-    }
 
     azuread = {
       version = "~> 2.15.0"
     }
   }
   backend "azurerm" {}
+}
+
+provider "azurerm" {
+  version = "~> 2.94"
+  features {}
+  subscription_id = var.sub_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
