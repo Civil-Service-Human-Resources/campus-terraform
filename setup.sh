6c8066bb769aed6e272db9760aa2edb28ae15e51
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2022-03-22 14:49:25 +0000

    udpated readme

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ echo "Validting env vars"
 : "${RESOURCE_GROUP_NAME?Please set a valid RESOURCE_GROUP_NAME}"
 : "${STORAGE_ACCOUNT_NAME:?Please set a valid STORAGE_ACCOUNT_NAME}"
 : "${KEY_SUFFIX:?Please set a valid KEY_SUFFIX}"
+: "${CONTAINER_NAME:?Please set a valid CONTAINER_NAME}"
 
 echo "Getting account key for storage account"
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
