047da03870d7486d767b5fc23e367fb7fe8d24c7
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2022-03-29 09:43:38 +0100

    updated webops state

--- environments/webops/state.tf
+++ environments/webops/state.tf
@@ -1,4 +1,15 @@
 terraform {
   required_version = "~> 0.13.0"
+  required_providers {
+
+    azuread = {
+      version = "~> 2.15.0"
+    }
+  }
   backend "azurerm" {}
+}
+
+provider "azurerm" {
+  version = "~> 2.94"
+  features {}
 }
