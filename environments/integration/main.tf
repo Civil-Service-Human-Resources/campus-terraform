370be0586e4a036fc9c30b78bc4c428df1f1ef46
SavvasCO <savvas.stephanides@cabinetoffice.gov.uk>
2023-12-06 14:13:36 +0000

    Decommission Campus services

--- environments/integration/main.tf
+++ environments/integration/main.tf
@@ -32,20 +32,20 @@ data "azurerm_container_registry" "acr" {
   resource_group_name = "rg-campus-webops"
 }
 
-module "monitoring" {
-  source = "../../module/monitoring"
-  env = local.env
-  tags = local.tags
-}
+# module "monitoring" {
+#   source = "../../module/monitoring"
+#   env = local.env
+#   tags = local.tags
+# }
 
-module "app_secrets_vault" {
-  source = "../../module/keyvault"
-  deploy_group_name = var.deploy_group_name
-  kv_name = local.kv_name
-  kv_rg_name = local.kv_rg_name
-  secrets = local.secrets
-  tags = local.tags
-}
+# module "app_secrets_vault" {
+#   source = "../../module/keyvault"
+#   deploy_group_name = var.deploy_group_name
+#   kv_name = local.kv_name
+#   kv_rg_name = local.kv_rg_name
+#   secrets = local.secrets
+#   tags = local.tags
+# }
 
 module "campus_ui" {
   source = "../../module/apps/campus_ui"
@@ -55,24 +55,24 @@ module "campus_ui" {
   tags = local.tags
 }
 
-module "campus_service" {
-  source = "../../module/apps/campus_service"
-  depends_on = [
-    module.app_secrets_vault
-  ]
-  env = local.env
-  sku_tier = "Standard"
-  sku_size = "S1"
-  sku_capacity = 1
-  secrets_kv_name = local.kv_name
-  secrets_kv_rg_name = local.kv_rg_name
-  app_settings = {"APPLICATIONINSIGHTS_CONNECTION_STRING": module.monitoring.appi_conn_string}
-  acr_rg = data.azurerm_container_registry.acr.resource_group_name
-  acr_name = data.azurerm_container_registry.acr.name
-  app_dns_zone_name = local.app_dns_zone_name
-  app_dns_zone_rg_name = local.app_dns_zone_rg_name
-  content_cache_capacity = 1
-  content_cache_family = "C"
-  content_cache_sku = "Basic"
-  tags = local.tags
-}
+# module "campus_service" {
+#   source = "../../module/apps/campus_service"
+#   depends_on = [
+#     module.app_secrets_vault
+#   ]
+#   env = local.env
+#   sku_tier = "Standard"
+#   sku_size = "S1"
+#   sku_capacity = 1
+#   secrets_kv_name = local.kv_name
+#   secrets_kv_rg_name = local.kv_rg_name
+#   app_settings = {"APPLICATIONINSIGHTS_CONNECTION_STRING": module.monitoring.appi_conn_string}
+#   acr_rg = data.azurerm_container_registry.acr.resource_group_name
+#   acr_name = data.azurerm_container_registry.acr.name
+#   app_dns_zone_name = local.app_dns_zone_name
+#   app_dns_zone_rg_name = local.app_dns_zone_rg_name
+#   content_cache_capacity = 1
+#   content_cache_family = "C"
+#   content_cache_sku = "Basic"
+#   tags = local.tags
+# }
